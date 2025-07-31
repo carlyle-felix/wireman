bb2e1db9b4aae73616f6677cebdb8c285039c563
carlyle-felix <carlyle.felix@gmail.com>
2025-08-01 01:29:10 +0200

    fix: mkdir modes and don't use chdir in util.c

--- src/util.c
+++ src/util.c
@@ -23,6 +23,11 @@ int config_home(void)
     strcpy(p, home);
     wireman = strcat(p, "/.config/wireman/");       // initialize global wireman path.
 
+    // create ~/.config/wireman if it doesn't exist.
+    if (!is_dir(wireman)) {
+        mkdir(wireman, 0777);
+    } 
+
     return 0;
 }
 
@@ -39,8 +44,7 @@ char *config_path(char *dir)
         printf("error: failed to allocate memory for ");
         return NULL;
     }
-    strcpy(p, wireman);
-    strcat(p, dir);
+    sprintf(p, "%s%s", wireman, dir);
 
     return p;
 }
@@ -52,18 +56,18 @@ int is_dir(char *dir)
     
     p = config_path(dir);
     if (!p) {
-        printf("%s directory pointer.\n", dir);
+        printf("%s in is_dir().\n", dir);
         return 1;
     }
 
     d = opendir(p);
     free(p);
-    if (d) {
+    if (!d) {
         closedir(d);
-        return 1;       // true.
+        return 0;
     }
 
-    return 0;
+    return 1;
 }
 
 /*
@@ -77,56 +81,33 @@ int store_key(char *key_name, char *key_type, char *key)
     char *filename;
     int res;
 
-    // cd into ~/.conf/wireman/.
-    p = config_path(wireman);
-    if (!p) {
-        printf("%s directory pointer.\n", wireman);
-        return 1;
-    }
-    res = chdir(p);
-    printf("no prob\n");
-    if (!res) {
-        printf("error: failed to open dir %s\n", wireman);
-        return res;
-    }
-    free(p);
-    
+    // check for ~/.config/wireman/<key_name>, create if doesn't exist.
     p = config_path(key_name);
     if (!p) {
         printf("%s.\n", key_name);
-        free(p);
         return 1;
-    } else if (is_dir(p)) {
-        mkdir(p, 755);
-    }
-
-    res = chdir(p);
-    if (!res) {
-        printf("error: failed to open dir %s\n", key_name);
-        return res;
+    } else if (!is_dir(p)) {
+        mkdir(p, 0777);
     }
     free(p);
 
-
-    p = malloc(strlen(wireman) + strlen(key_name) + 1 + strlen(key_type) + 1);
+    // get absolute file path
+    p = malloc(strlen(wireman) + (strlen(key_name) * 2) + 2 + strlen(key_type) + 1);
     if (!p) {
-        printf("error: failed to allocate memory for %s.%s", key_name, key_type);
+        printf("error: failed to allocate memory for %s.%s\n", key_name, key_type);
         return 1;
     }
-    strcpy(p, wireman);
-    strcat(p, key_name);
-    strcat(p, ".");
-    strcat(p, key_type);
-    f = fopen(p, "w");
-    if (!f) {
-        printf("error: failed to create %s%s", key_name, key_type);
-        return 1;
-    }
-    fprintf(f, key, NULL);
-    fclose(f);
-    free(p);
+    sprintf(p, "%s%s/%s.%s", wireman, key_name, key_name, key_type);
 
-    chdir(home);        // TODO: change to pwd.
+    // write base64
+    f = fopen(p, "w");
+    free(p);
+    if (!f) {
+        printf("error: failed to create %s.%s\n", key_name, key_type);
+        return 1;
+    }
+    fprintf(f, key);
+    fclose(f);
 
     return 0;
 }
